b24e7643c23d80b74241f25a0d41af74dcd55e10
evapiskun <105502885+evapiskun@users.noreply.github.com>
2024-02-06 11:55:57 -0500

    Database working with fake data

--- attendancePage/routes.py
+++ attendancePage/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from .models import Student
 
 
@@ -7,9 +7,13 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/', methods=('GET', 'POST'))
 def main():
+    return render_template(
+        'index.html'
+    )
+
+@bp.route('/database')
+def database():
     output = ""
     for u in Student.query.all():
         output += str(u.__dict__) + "\n"
     return output
-
-@bp.route('/database')
